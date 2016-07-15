ffcf1486bad0fa5db967522450946570c3c36c18
Ryan Kennedy <rkennedy@2ndwatch.com>
2016-07-14 23:59:28 -0700

    removed unecessary provider vars

--- variables.tf
+++ variables.tf
@@ -38,9 +38,3 @@ variable "azs" {
   default = ""
   description = "Comma separated list of Availability Zones"
 }
-
-// Variables for providers used in this module
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-
